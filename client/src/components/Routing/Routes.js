35a55c0e0c50d6d220cf766cc0d1241cebf0274a
Antonio <antonio_97@live.se>
2019-08-21 21:41:12 +0200

    added pricing route

--- client/src/components/Routing/Routes.js
+++ client/src/components/Routing/Routes.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from '../Home/Home';
+import Pricing from '../Pricing/Pricing';
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path='/' component={Home} />
+      <Route exact path='/pricing' component={Pricing} />
     </Switch>
   );
 };
