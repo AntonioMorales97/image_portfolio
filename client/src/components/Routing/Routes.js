2c80ebc3b9dc4cf1c2a95b5cbbdeee9d1f62420b
Antonio <antonio_97@live.se>
2019-08-22 16:33:49 +0200

    added contact route and redirect to home if no match

--- client/src/components/Routing/Routes.js
+++ client/src/components/Routing/Routes.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import Home from '../Home/Home';
 import Pricing from '../Pricing/Pricing';
+import Contact from '../Contact/Contact';
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact path='/' component={Home} />
       <Route exact path='/pricing' component={Pricing} />
+      <Route exact path='/contact' component={Contact} />
+      <Route render={() => <Redirect to='/' />} />
     </Switch>
   );
 };
